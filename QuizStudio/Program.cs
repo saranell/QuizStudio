04764e3a6c22b18fd661baeb24a8f5f5c2eea42e
saranell <saranell92@gmail.com>
2022-09-09 15:57:03 -0500

    creating classes

--- QuizStudio/Program.cs
+++ QuizStudio/Program.cs
@@ -1,5 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
-//writing something new
-//testtesttest
+namespace QuizStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
